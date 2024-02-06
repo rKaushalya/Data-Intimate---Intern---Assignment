ea1b47efe543b63bb8884e4fca76dfe055ed0b9d
rKaushalya <ravinduk56@gmail.com>
2024-02-06 22:54:21 +0530

    database connection error fixed

--- db/DBConnection.js
+++ db/DBConnection.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '1234',
+    user: 'sqluser',
+    password: 'password',
     database: 'data_intimate'
 });
 
