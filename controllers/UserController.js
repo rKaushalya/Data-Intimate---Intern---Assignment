ee2297cd967be0a5421a8072c2313062b3ccdfd5
rKaushalya <ravinduk56@gmail.com>
2024-02-08 00:55:41 +0530

    jwt refresh token added

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -27,9 +27,29 @@ const UserController = {
                 if (error) return res.json({message: "error checking data.! "+error});
                 //jwt authentication
                 const user = {username:name};
-                const token = jwt.sign(user,process.env.AUTH_TOKEN);
-                res.send({token});
+                const token = jwt.sign(user,process.env.AUTH_TOKEN,{expiresIn: '10s'});
+                const refreshToken = jwt.sign(user, process.env.RE_AUTH_TOKEN, {expiresIn: '24h'});
+                database.query("INSERT INTO refreshToken VALUES (?)",[refreshToken],(e,t)=>{
+                    if (e) return res.json({message: "error insert token.! "+e});
+                    console.log("refresh token added to database.!");
+                });
+                res.send({token,refreshToken});
             })
+        }catch (e) {
+            console.error(e);
+            res.status(500).json({error : 'Something went wrong.!'});
+        }
+    },
+    getToken : function (req,res,next) {
+        try {
+            const refreshToken = req.body.refreshToken;
+            if (refreshToken==null) res.sendStatus(401);
+            if (!refreshToken.includes(refreshToken)) res.sendStatus(403);
+            jwt.verify(refreshToken,process.env.RE_AUTH_TOKEN,(error,user)=>{
+                if (error) res.sendStatus(403);
+                const token = jwt.sign({username:user.username},process.env.AUTH_TOKEN,{expiresIn: '10s'});
+                res.send({token});
+            });
         }catch (e) {
             console.error(e);
             res.status(500).json({error : 'Something went wrong.!'});
@@ -37,7 +57,12 @@ const UserController = {
     },
     logOut : function (req, res, next) {
         try {
-            const sql = "";
+            const refreshToken = req.body.refreshToken;
+            const sql = "DELETE FROM refreshToken WHERE  token=?";
+            database.query(sql,[refreshToken],(error,result)=>{
+                if (error) return res.json({message: "error insert token.! "+error});
+                res.sendStatus(204);
+            });
         }catch (e) {
             console.error(e);
             res.status(500).json({error : 'Something went wrong.!'});
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const UserController = require('../controllers/UserController');
 
 /* GET users listing. */
 router.post("/save",UserController.saveUser);
+router.post("/token",UserController.getToken);
 router.get("/check",UserController.checkUser);
+router.delete("/delete",UserController.logOut);
 
 module.exports = router;
